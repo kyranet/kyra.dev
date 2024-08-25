d26c3bc99d6be577ece5a02dd835badff7dc8fbd
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-08-25 12:03:23 -0400

    fix: clamp percentage to prevent overflow

--- components/user/card-seekbar.vue
+++ components/user/card-seekbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="mt-3 text-xs">
 		<div class="h-1 rounded-xl bg-neutral-700">
-			<div class="bg-primary h-1 rounded-xl bg-white" :style="{ width: `${percentage}%` }"></div>
+			<div class="bg-primary h-1 rounded-xl bg-white" :style="{ width: `${clampedPercentage}%` }"></div>
 		</div>
 		<div class="flex items-start justify-between">
 			<div class="mb-0 mt-0.5 py-0">{{ currentPosition }}</div>
@@ -23,4 +23,5 @@ const currentDateTime = useNow();
 const currentPosition = computed(() => formatTime(props.startTimeMs, currentDateTime.value.getTime()));
 const songDuration = computed(() => formatTime(props.startTimeMs, props.endTimeMs));
 const percentage = computed(() => ((currentDateTime.value.getTime() - props.startTimeMs) / (props.endTimeMs - props.startTimeMs)) * 100);
+const clampedPercentage = computed(() => Math.min(Math.max(percentage.value, 0), 100));
 </script>
